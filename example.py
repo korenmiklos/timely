94bd802bb3f6913506f93a2b14cc24e7dba91b19
Miklos Koren <miklos.koren@gmail.com>
2015-11-12 09:39:01 +0100

    a more advanced example, counting number of concurrent spells

--- example.py
+++ example.py
@@ -6,10 +6,20 @@ year_1996 = timely.Interval((start_date, end_date))
 
 worker_spell = timely.Interval(('1996-11-01', '2009-06-30'))
 period_worked_in_1996 = worker_spell.intersection(year_1996)
-worker_joined_in_1996 = worker_spell.beginnin in year_1996
+worker_joined_in_1996 = worker_spell.beginning in year_1996
 
 other_worker_spell = timely.Interval(('2007-01-01', '')) # open spell
 two_workers_overlapped = worker_spell.intersects(other_worker_spell)
 
 sample_period = timely.MultiInterval([year_1996, worker_spell, other_worker_spell]).envelope
 # = Interval(('1996-01-01', ''))
+
+# A more advanced example. Count the number of workers at the firm over time.
+worker_spells = [worker_spell, other_worker_spell]
+sample_period = timely.MultiInterval(worker_spells).envelope
+events = [spell.beginning for spell in worker_spells] + [spell.end for spell in worker_spells]
+segments = sample_period.segment_by_instant(events)
+
+output = []
+for segment in segments:
+	output.append(dict(time_period=segment, number_of_workers=sum([1 for spell in worker_spells if spell.intersects(segment)])))
